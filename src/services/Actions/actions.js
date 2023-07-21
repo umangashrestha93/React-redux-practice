08df0db2a003375e037fa5711fb829726c8da74d
Umanga Shrestha <umangashrestha93@gmail.com>
2023-07-21 17:38:27 +0545

    actions in redux

--- src/services/Actions/actions.js
+++ src/services/Actions/actions.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import { ADD_TO_CART } from "../constant";
 
-function actions() {
-  return (
-    <div>
-      <h1>action</h1>
-    </div>
-  )
+export const addToCart = (data)=>{
+    return{
+        type: ADD_TO_CART,
+        data: data
+    }
+
 }
-
-export default actions
